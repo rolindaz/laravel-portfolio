efde73b8a51caf11f838474cf42336d7eb0dcd4f
rolindaz <rolinda.zamboni@gmail.com>
2026-01-06 18:02:58 +0100

    feat: Add action section with button to create new project in index view

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,6 +2,19 @@
 
 @section('title', 'I miei progetti')
 
+@section('actions')
+
+<div class="m-4">
+    <a href="{{ route('projects.create') }}">
+            <button class="btn btn-success">
+                Aggiungi un nuovo progetto
+            </button>
+    </a>  
+</div>
+
+
+@endsection
+
 @section('content')
 <ul>
         @foreach ($projects as $project)
