5a88313cde9e64a0717e752f14670ee1504ddbdf
rolindaz <rolinda.zamboni@gmail.com>
2026-01-06 18:00:15 +0100

    feat: Enhance project deletion with confirmation modal in show view

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -10,14 +10,40 @@
         </button>
     </a>  
     
-    <form action="{{ route('projects.destroy', $project) }}" method="POST">
-        @csrf
-        @method('DELETE')
-        <button type="submit" class="btn btn-danger">
-            Elimina
-        </button>
-    </form> 
+    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Elimina
+    </button>
 </div>
+
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">
+            Conferma eliminazione
+        </h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        Sei sicuro di voler eliminare questo progetto? L'azione è irreversibile.
+      </div>
+      <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+              Annulla
+          </button>
+        <form action="{{ route('projects.destroy', $project) }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-danger">
+                Elimina definitivamente
+            </button>
+        </form> 
+    </div>
+</div>
+</div>
+</div>
+
 @endsection
 
 @section('content')
