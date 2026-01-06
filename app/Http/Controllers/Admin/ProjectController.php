0d7bcecc73de718c78b4214b951edc50082ffbd7
rolindaz <rolinda.zamboni@gmail.com>
2026-01-06 16:55:27 +0100

    feat: Implement edit view for project with form submission

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -57,9 +57,9 @@ class ProjectController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Project $project)
     {
-        //
+        return 'this is the edit form page';
     }
 
     /**
