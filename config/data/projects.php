b0879667d9a8e1e71bb4f8957444a46b8562bf63
rolindaz <rolinda.zamboni@gmail.com>
2026-01-08 15:38:18 +0100

    feat: Add initial projects data with titles, overviews, and links for various HTML/CSS practice exercises

---
+++ config/data/projects.php
@@ -0,0 +1,60 @@
+<?php
+
+return [
+    [
+        "type_id" => "",
+        "title" => "Spotify Web",
+        "overview" => "Spotify Web interface reproduction as an HTML/CSS practice exercise. No interactive functions were implemented. It just looks cute!",
+        "repo_link" => "https://github.com/rolindaz/html-css-spotifyweb",
+        "view_link" => "https://html-preview.github.io/?url=https://github.com/rolindaz/html-css-spotifyweb/blob/main/index.html",
+        "image" => ""
+    ],
+    [
+        "type_id" => "",
+        "title" => "Giallo Booleano",
+        "overview" => "Giallo Zafferano’s sample Tiramisù recipe page reproduction as an HTML/CSS practice exercise for responsive designs. No interactive functions were implemented, except for a video preview.",
+        "repo_link" => "https://github.com/rolindaz/htmlcss-responsive-layout/blob/main/index.html",
+        "view_link" => "https://html-preview.github.io/?url=https://github.com/rolindaz/htmlcss-responsive-layout/blob/main/index.html",
+        "image" => ""
+    ],
+    [
+        "type_id" => "",
+        "title" => "Boolando",
+        "overview" => "Zalando products page reproduction as an HTML/CSS practice exercise for e-commerce websites. No interactive functions were implemented.",
+        "repo_link" => "https://github.com/rolindaz/html-css-boolando",
+        "view_link" => "https://html-preview.github.io/?url=https://github.com/rolindaz/html-css-boolando/blob/main/index.html",
+        "image" => ""
+    ],
+    [
+        "type_id" => "",
+        "title" => "Dropbox",
+        "overview" => "Dropbox business plans page reproduction as an HTML/CSS practice exercise. No interactive functions were implemented.",
+        "repo_link" => "https://github.com/rolindaz/htmlcss-dropbox",
+        "view_link" => "https://html-preview.github.io/?url=https://github.com/rolindaz/htmlcss-dropbox/blob/main/index.html",
+        "image" => ""
+    ],
+    [
+        "type_id" => "",
+        "title" => "DC Comics",
+        "overview" => "A DC Comics catalog page reproduction used to practice HTML, CSS, React and Node.js. No interactive functions were implemented.",
+        "repo_link" => "https://github.com/rolindaz/react-dc-comics",
+        "view_link" => "https://reactdccomics.netlify.app",
+        "image" => ""
+    ],
+    [
+        "type_id" => "",
+        "title" => "BooRoad Travel",
+        "overview" => "A small app to create travels and add participants to them, a cooperative project built with React, Vite, JavaScript, HTML, CSS.",
+        "repo_link" => "https://github.com/edoardolova/BooRoad-travel",
+        "view_link" => "https://booroad-travel.netlify.app/",
+        "image" => ""
+    ],
+    [
+        "type_id" => "",
+        "title" => "Click'n'Cook",
+        "overview" => "- A complete food-based e-commerce that was presented as final project for my web development course. A cooperative effort made with MySql, React, JavaScript, Express, Node, HTML, CSS.",
+        "repo_link" => "",
+        "view_link" => "",
+        "image" => ""
+    ],
+];
